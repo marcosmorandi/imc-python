761f446058179897c85893a93665e3bfd972cc0f
marcosmorandi <marcosamorandi@gmail.com>
2023-01-24 17:52:01 -0300

    Comentário com a tabela e fórmula.

--- imc.py
+++ imc.py
@@ -5,3 +5,17 @@ print("========================")
 nome = str(input("\nQual seu primeiro nome: "))
 altura = float(input("\nQual a sua altura em cm: "))
 peso = float(input("\nQual seu peso atual em kg: "))
+
+# Apagar depois de pronto:
+'''
+Formula: quilos / (altura * altura)
+
+    Resultado            |Situação
+    Abaixo de 17         |Muito abaixo do peso
+    Entre 17 e 18,49     |Abaixo do peso
+    Entre 18,50 e 24,99  |Peso normal
+    Entre 25 e 29,99     |Acima do peso
+    Entre 30 e 34,99     |Obesidade I
+    Entre 35 e 39,99     |Obesidade II (severa)
+    Acima de 40          |Obesidade III (mórbida)
+'''
