633e97e0cff0694ef7f6985057c9a79a47235bb4
marcosmorandi <marcosamorandi@gmail.com>
2023-01-25 10:47:04 -0300

    Formatado o resultado do IMC para 2 casas decimais.

--- imc.py
+++ imc.py
@@ -3,9 +3,9 @@ print("=       Cálculo do IMC       =")
 print("==============================")
 
 nome = str(input("\nQual seu nome? "))
-altura = float(input("\nQual sua altura " + nome +"? "))
-peso = float(input("\nQual seu peso atual " + nome +"? "))
+altura = float(input("\nQual sua altura em metros(m) " + nome +"? "))
+peso = float(input("\nQual seu peso atual em quilogramas(kg) " + nome +"? "))
 
 imc  = peso / (altura * altura)
 
-print("\n" + nome + ", seu IMC atual é:", + imc ,"\n")
+print("\n" + nome + ", seu IMC atual é: {:.2f}".format(imc) + "\n")
