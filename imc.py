62d2d208abe7511e55e388ef3e6486cee15a9a22
marcosmorandi <marcosamorandi@gmail.com>
2023-01-25 11:57:58 -0300

    Criando as estruturas condicionais.

--- imc.py
+++ imc.py
@@ -8,7 +8,7 @@ peso = float(input("\nQual seu peso atual em quilogramas(kg)? "))
 
 imc  = peso / (altura * altura)
 
-print("\n" + nome + ", seu IMC atual é: {:.2f}".format(imc) + "\n")
+print("\n" + nome + ", seu IMC atual é: {:.2f}".format(imc) + "\n") # "{:.2f}" junto com o ".format()" formata para que apareça 2 casas decimais.
 
 if imc < 17: # Depois do primeiro "if' usa-se "elif" para economizar processamento, caso a primeira condição seja verdadeira, ele não processa nada mais abaixo.
     print("Você está muito abaixo do peso normal.")
@@ -25,13 +25,3 @@ elif imc == 35 and imc <= 39.99:
 elif imc > 40:
     print("Você está com obesidade III, mórbida.")
 # "else" no final é opcional, ele não recebe parametro nenhum, fica "else:".
-
-'''
-    Abaixo de 17         |Muito abaixo do peso
-    Entre 17 e 18,49     |Abaixo do peso
-    Entre 18,50 e 24,99  |Peso normal
-    Entre 25 e 29,99     |Acima do peso
-    Entre 30 e 34,99     |Obesidade I
-    Entre 35 e 39,99     |Obesidade II (severa)
-    Acima de 40          |Obesidade III (mórbida)
-'''
