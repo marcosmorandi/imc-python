79b22e5c9880b9f7dedf6572d6ff2c465c17ab7f
marcosmorandi <marcosamorandi@gmail.com>
2023-01-25 09:11:55 -0300

    Recomeçando o projeto.

--- imc.py
+++ imc.py
@@ -1,25 +1,6 @@
-print("\n========================")
-print("=    Cálculo do IMC    =")
-print("========================")
+altura = float(input("Qual sua altura? "))
+peso = float(input("Qual seu peso atual? "))
 
-nome = str(input("\nQual seu nome: "))
-altura = float(input("\nQual a sua altura em cm: "))
-peso = float(input("\nQual seu peso atual em kg: "))
+imc  = peso / (altura * altura)
 
-imc = peso / (altura * altura)
-
-
-
-# Apagar depois de pronto:
-'''
-Fórmula: quilos / (altura * altura)
-
-    Resultado            |Situação
-    Abaixo de 17         |Muito abaixo do peso
-    Entre 17 e 18,49     |Abaixo do peso
-    Entre 18,50 e 24,99  |Peso normal
-    Entre 25 e 29,99     |Acima do peso
-    Entre 30 e 34,99     |Obesidade I
-    Entre 35 e 39,99     |Obesidade II (severa)
-    Acima de 40          |Obesidade III (mórbida)
-'''
+print("Seu IMC ", + imc)
