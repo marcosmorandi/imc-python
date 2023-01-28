2a36856659d90024fed517c142b813bf7bcf1179
marcosmorandi <marcosamorandi@gmail.com>
2023-01-28 14:01:09 -0300

    Cores nas condicionais.

--- imc.py
+++ imc.py
@@ -1,7 +1,7 @@
 print("\n==============================================")
 print("=               Cálculo do IMC               =")
 print("==============================================")
-print("* Use '.' ao invés de ',' para casas decimais.")
+print('* Use "." ao invés de "," para casas decimais.') # Se for usar aspas duplas ou simples no texto exibido, não use o mesmo tipo no comando.
 
 nome = str(input("\nQual seu nome? "))
 altura = float(input("\nQual sua altura em metros(m)? "))
@@ -38,4 +38,35 @@ Testes das condicionais:
 1.75m, 95kg = 4º elif [ok] - vermelho []
 1.75m, 110kg = 5º elif [ok] - vermelho []
 1.75m, 130kg = 6º elif [ok] - vermelho []
+
+Cores ANSI escape sequance
+\033[0;33;44m
+\033[(estilo);(cor do texto);(cor do fundo)m
+"\033[" abre, "m" fecha.
+
+Estilos:
+0 - Nenhum
+1 - Negrito
+4 - Sublinhado
+7 - Negativo, inverte texto e fundo.
+
+Cor do Texto:
+30 - Branco
+31 - Vermelho
+32 - Verde
+33 - Amarelo
+34 - Azul
+35 - Roxo
+36 - Ciano
+37 - Cinza
+
+Cor do Fundo:
+40 - Branco
+41 - Vermelho
+42 - Verde
+43 - Amarelo
+44 - Azul
+45 - Roxo
+46 - Ciano
+47 - Cinza
 '''
