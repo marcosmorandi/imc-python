1dc887940f40b0fc828f434285d6ce902722c0a7
marcosmorandi <marcosamorandi@gmail.com>
2023-01-28 15:45:34 -0300

    Destacando e colorindo fundo do resultado em texto do IMC.

--- imc.py
+++ imc.py
@@ -14,35 +14,38 @@ print("\n" + nome + ", seu IMC atual é: {:.2f}".format(imc) + "\n")
 
 # Depois do primeiro "if' usa-se "elif" para economizar processamento, caso a primeira condição seja verdadeira, ele não processa nada mais abaixo.
 if imc < 17:
-    print("Você está muito abaixo do peso normal.\n") # Colocar cor vermelha.
+    print("\033[1;41mVocê está muito abaixo do peso normal.\033[m\n") # Fundo vermelho, texto em negrito. Fechar o escape sequence para não colorir o que vem depois.
 elif imc >= 17 and imc <= 18.49:
-    print("Você está abaixo do peso normal.\n") # Colocar cor amarela.
+    print("\033[1;43mVocê está abaixo do peso normal.\033[m\n") # Fundo amarelo, texto em negrito.
 elif imc >= 18.50 and imc <= 24.99:
-    print("Você está com o peso normal.\n") # Colocar cor verde.
+    print("\033[1;42mVocê está com o peso normal.\033[m\n") # Fundo verde, texto em negrito.
 elif imc >= 25 and imc <= 29.99:
-    print("Você está acima do peso normal.\n") # Colocar cor amarela.
+    print("\033[1;43mVocê está acima do peso normal.\033[m\n") # Fundo amarelo, texto em negrito.
 elif imc >= 30 and imc <= 34.99:
-    print("Você está com obesidade I.\n") # Colocar cor vermelha.
+    print("\033[1;41mVocê está com obesidade I.\033[m\n") # Fundo vermelho, texto em negrito.
 elif imc >= 35 and imc <= 39.99:
-    print("Você está com obesidade II, severa.\n") # Colocar cor vermelha.
+    print("\033[1;41mVocê está com obesidade II, severa.\033[m\n") # Fundo vermelho, texto em negrito.
 elif imc > 40:
-    print("Você está com obesidade III, mórbida.\n") # Colocar cor vermelha.
+    print("\033[1;41mVocê está com obesidade III, mórbida.\033[m\n") # Fundo vermelho, texto em negrito.
 # "else" no final é opcional, ele não recebe parametro nenhum, fica "else:".
 
 '''
 Testes das condicionais:
-1.75m, 50kg = if [ok] - vermelho []
-1.75m, 55kg = 1º elif [ok] - amarelo []
-1.75m, 70kg = 2º elif [ok] - verde []
-1.75m, 80kg = 3º elif [ok] - amarelo []
-1.75m, 95kg = 4º elif [ok] - vermelho []
-1.75m, 110kg = 5º elif [ok] - vermelho []
-1.75m, 130kg = 6º elif [ok] - vermelho []
+1.75m, 50kg = if [ok] = fundo vermelho, texto em negrito [ok]
+1.75m, 55kg = 1º elif [ok] = fundo amarelo, texto em negrito [ok]
+1.75m, 70kg = 2º elif [ok] = fundo verde, texto em negrito [ok]
+1.75m, 80kg = 3º elif [ok] = fundo amarelo, texto em negrito [ok]
+1.75m, 95kg = 4º elif [ok] = fundo vermelho, texto em negrito [ok]
+1.75m, 110kg = 5º elif [ok] = fundo vermelho, texto em negrito [ok]
+1.75m, 130kg = 6º elif [ok] = fundo vermelho, texto em negrito [ok]
 
-Cores ANSI escape sequance
-\033[0;33;44m
+
+Cores ANSI escape sequence
 \033[(estilo);(cor do texto);(cor do fundo)m
+\033[0;33;44m
 "\033[" abre, "m" fecha.
+Não tem necessidade de definir os 3 parâmetros, se tiver mais de um separe com ";"
+Não esqueça de fechar com "\033[m" para não colorir informações posteriores.
 
 Estilos:
 0 - Nenhum
